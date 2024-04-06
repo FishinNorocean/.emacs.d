e85e427b0b095c952c62152c6c790606bc212ccd
Fishin Norocean <swklouis@gmail.com>
2024-04-07 00:09:24 +0800

    dirvish added

--- init.el
+++ init.el
@@ -15,6 +15,8 @@
 (setenv "EMACS_ZSH" "true")
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
+(if *is-a-mac* (defconst *trash-dir* "~/.Trash/")
+  (defconst *trash-dir* "~/.local/share/Trash"))
 
 ;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
@@ -153,6 +155,50 @@
   (("C-." . embark-act)         ;; pick some comfortable binding
    ("C-;" . embark-dwim)        ;; good alternative: M-.
    ("C-h B" . embark-bindings))) ;; alternative for `describe-bindings'
+
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("d" "~/Downloads/"                "Downloads")
+     ;("m" "/mnt/"                       "Drives")
+	 ("p" "~/projects/"                 "Projects")
+     ;; ("t" *trash-dir*                   "TrashCan")
+	 ))
+  :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format
+        '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-attributes
+        '(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
+  ;; (setq delete-by-moving-to-trash t)
+  (if *is-a-mac* (setq dired-listing-switches "-l -A -h")
+	(setq dired-listing-switches
+          "-l --almost-all --human-readable --group-directories-first --no-group"))
+  :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
+  (("C-c f" . dirvish-fd)
+   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
+   ("a"   . dirvish-quick-access)
+   ("f"   . dirvish-file-info-menu)
+   ;("y"   . dirvish-yank-menu)
+   ;("N"   . dirvish-narrow)
+   ("^"   . dirvish-history-last)
+   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
+   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+   ; ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-l" . dirvish-ls-switches-menu)
+   ("M-m" . dirvish-mark-menu)
+   ("M-t" . dirvish-layout-toggle)
+   ("M-s" . dirvish-setup-menu)
+   ("M-e" . dirvish-emerge-menu)
+   ;; ("M-j" . dirvish-fd-jump)
+   ))
 
 (use-package avy
   :ensure t
@@ -643,7 +689,10 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
 
 (use-package undo-tree
   :ensure t
-  :config (global-undo-tree-mode))
+  :config
+  (global-undo-tree-mode)
+  :custom
+  (undo-tree-auto-save-history nil))
 ;; (if *is-a-mac* 
 ;;     (use-package undo-tree
 ;;       :ensure t
